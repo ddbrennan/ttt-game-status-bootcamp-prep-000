f197907aa7ef8319199f10dad3c6396dc1c18f23
David Brennan <ddbren@gmail.com>
2017-11-09 22:23:17 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,6 +27,8 @@ def won?(board)
 
     if pos_1 == "X" && pos_2 == "X" && pos_3 == "X"
       return combo
+    elsif pos_1 == "O" && pos_2 == "O" && pos_3 == "O"
+      return combo
     else
       false
     end
